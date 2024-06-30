a115fb5741639108e5ef85f2a6b56508390838cf
Selznick, Richard <richardselz@gmail.com>
2024-06-30 13:40:32 -0700

    Rustling Exercises in Progress

---
+++ variables/variables6.rs
@@ -0,0 +1,9 @@
+// variables6.rs
+//
+// Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
+// hint.
+
+fn main() {
+    let NUMBER = 3;
+    println!("Number {}", NUMBER);
+}
